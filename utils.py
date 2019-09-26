65fe704ea147899fb6023d4c0bc5cfce735c511e
spherebleue <7spherebleue7@gmail.com>
2019-09-26 17:19:25 -0400

    determinek_i

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 
-# Notations : 
+# Notations :
 # Li / Ri : désigne le message de 6 bits Li ou Ri (itération i)
 # Li_m1 / Ri_m1 : désigne Li et Ri à l'itération i-1
 # Li_p1 / Ri_p1 : désigne Li et Ri à l'itération i+1
@@ -12,7 +12,7 @@ def iterationDES(Li_m1, Ri_m1, Ki):
     return 0
 
 def f(Li_m1, Ki):
-    #Li+_ =  
+    #Li+_ =
 
     return 0
 
@@ -32,3 +32,17 @@ def xor(chaine1, chaine2):
             res_bit = '1'
         res_chaine += res_bit
     return res_chaine
+
+# key is 9 bits
+# iteration is the current iteration number (int)
+def determineK_i(key, iteration):
+    key_iteration = ''
+    for i in range(iteration, min(len(key), iteration + 8 ) ) :
+        key_iteration += key[i]
+    i = 0
+    while len(key_iteration != 8) :
+        key_iteration += key[i]
+        i += 1
+    return key_iteration
+
+determineK_i("010101010",0 )
