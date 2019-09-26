ebd399c03870ec1ec0c4307fd3b49507abc42be8
Spherebleue <7spherebleue7@gmail.com>
2019-09-27 00:33:22 +0200

    ex2 done

--- ex2.py
+++ ex2.py
@@ -7,11 +7,62 @@ from random import randint
 # Exercice 2
 print("Code par FERY Simon et DURAFFOURG Maud")
 print('Exercice 2')
+
 """
 Partie A
 """
+
+print('\nPartie A :')
+
+plaintext = ["011001111001",
+             "101001011010",
+             "010111010101",
+             "100001000101"]
+C0 = ''
+for i in range(0,12):
+    C0 += str(randint(0,1))
+key = generate_random_key()
+
+print("Le message a code est : " + " ".join(plaintext) )
+print("On utilise CO = " + C0 + " et la cle aleatoire est : " + key)
+
+# crypting
+cypher = CBC_crypting(plaintext, C0, key)
+print("Le cypher est : " + " ".join(cypher))
+
+# decrypting
+decode = CBC_decrypting(cypher, C0, key)
+print("Le message decode est : " + " ".join(decode))
 
 
 """
 Partie B
 """
+print('\nPartie B :')
+
+plaintext_1 = ["011000111111",
+             "101001011010",
+             "010010111101",
+             "111010011101"]
+
+# on change le 14 eme bit
+plaintext_2 = ["011000111111",
+             "111001011010",
+             "010010111101",
+             "111010011101"]
+
+#On reprend les cles et C0 precedent
+
+print("Le message 1 a code est : " + " ".join(plaintext_1) )
+print("Le message 2 a code est : " + " ".join(plaintext_2) )
+print("On utilise CO = " + C0 + " et la cle aleatoire est : " + key)
+
+# crypting
+cypher_1 = CBC_crypting(plaintext_1, C0, key)
+cypher_2 = CBC_crypting(plaintext_2, C0, key)
+print("Le cypher 1 est : " + " ".join(cypher_1))
+print("Le cypher 2 est : " + " ".join(cypher_2))
+print("On constate qu a partir du deuxieme paquet de donnees, le cypher 2 est tres different du cypher 1 avec "
+      "un bit de difference.")
+
+
--- utils.py
+++ utils.py
@@ -11,14 +11,25 @@ S1 = ['101','010','001','110','011','100','111','000',
 S2 = ['100','000','110','101','111','001','011','010',
       '101','011','000','111','110','010','001','100']
 
-def CBC(plaintext, key):
-    iteration_text = plaintext
-    for iteration in range(0,4):
-        # add xor
-        iteration_text = iterationDES(iteration_text, determineK_i(key, iteration))
+#CBC avec EK
+def CBC_crypting(plaintext, C0, key):
+    C_before = C0
+    cypher = []
+    for iteration in range(0, 4):
+        P_j = plaintext[iteration]
+        text_to_crypt = xor(P_j, C_before)
+        C_before = Ek(text_to_crypt, key)
+        cypher.append(C_before)
+    return cypher
 
-    return iteration_text
-
+def CBC_decrypting(cypher, C0, key):
+    C_before = C0
+    decode = []
+    for iteration in range(0, 4):
+        decrypted_text = decodeDES( cypher[iteration], key, 3 )
+        decode.append( xor(decrypted_text, C_before ) )
+        C_before = cypher[iteration]
+    return decode
 
 def Ekprime(plaintext,key):
     iteration_text = plaintext
