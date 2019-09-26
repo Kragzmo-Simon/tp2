3e8804ae783041832c27b582c5215041ee06451a
spherebleue <7spherebleue7@gmail.com>
2019-09-26 17:40:34 -0400

    test

--- ex1.py
+++ ex1.py
@@ -9,16 +9,25 @@ print('Exercice 1')
 """
 Partie A
 """
-L0 = '011011'
-R0 = '001100'
+# exemple vu dans le cours
+L0 = '011100'
+R0 = '100110'
 
-K = '100111010'
+K = '011001010'
 
 L0R0 = L0 + R0
+print(iterationDES(L0, R0, determineK_i(K, 0) ))
 
+# une iteration avec k0 + le dechiffrement avec k0
 
 """
 Partie B
+
+L0 = '011100'
+R0 = '100110'
+
+K = '01100101'
+
 """
 
 """
