3bfaaca644ccebf2f4a7d542ce6be114f16d49fc
Kragzmo-Simon <fery.simon@ymail.com>
2019-09-26 22:34:12 +0200

    few things

--- ex1.py
+++ ex1.py
@@ -1,4 +1,30 @@
 
+from utils import *
+
 # Exercice 1
 
 print('Exercice 1')
+
+
+"""
+Partie A
+"""
+L0 = '011011'
+R0 = '001100'
+
+K = '100111010'
+
+L0R0 = L0 + R0
+
+
+"""
+Partie B
+"""
+
+"""
+Partie C
+"""
+
+"""
+Partie D
+"""
--- utils.py
+++ utils.py
@@ -1,6 +1,26 @@
-# some utility functions
 
+# Notations : 
+# Li / Ri : désigne le message de 6 bits Li ou Ri (itération i)
+# Li_m1 / Ri_m1 : désigne Li et Ri à l'itération i-1
+# Li_p1 / Ri_p1 : désigne Li et Ri à l'itération i+1
 
-def jesuisutile():
+# transforme LiRi en Li+1/Ri+1 grâce à une clé K
+def iterationDES(Li_m1, Ri_m1, Ki):
+
+    Li = Ri_m1
+
+    # Ri = xor(Li_m1,f(Ri_m1, Ki))
 
     return 0
+
+def f(Li_m1, Ki):
+    #Li+_ =  
+
+    return 0
+
+def expander(chaine6bits):
+    chaine8bits = ''
+
+    return chaine8bits
+
+# Comment faire le xor sur nos chaines de bits?
